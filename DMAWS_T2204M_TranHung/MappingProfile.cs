d91cf21dc28e58e38f3cba6d59be4642ccb185d3
HungTranXD <105595047+HungTranXD@users.noreply.github.com>
2023-09-14 10:14:48 +0700

    first commit

---
+++ DMAWS_T2204M_TranHung/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DMAWS_T2204M_TranHung.DTOs;
+using DMAWS_T2204M_TranHung.Models;
+
+namespace DMAWS_T2204M_TranHung
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Project, ProjectDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeDTO>().ReverseMap();
+            CreateMap<ProjectEmployee, ProjectEmployeeDTO>().ReverseMap();
+        }
+    }
+}
